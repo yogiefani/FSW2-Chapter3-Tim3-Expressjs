89ecc5d04164da2ec58b7090e471bc831b032ff3
xkylz1 <fauzansaputra741@gmail.com>
2024-10-07 23:23:25 +0800

    feat: add delete category

--- server.js
+++ server.js
@@ -1,51 +1,85 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
-const { product } = require('./models')
+const { product } = require("./models");
+const { category } = require("./models");
 
-const PORT = 3000
+
+const PORT = 3000;
 
 // Body Parser
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use('/api/v1/products', async(req, res) => {
-    try{
-        const allProduct = await product.findAll()
+app.use("/api/v1/products", async (req, res) => {
+  try {
+    const allProduct = await product.findAll();
 
-        res.status(200).json({
-            "status": true,
-            "message": "get All Product Successfully!",
-            "total": allProduct.length,
-            "data": allProduct
-        })
-    } catch(error){
-        return res.status(500).json({
-            "status": false,
-            "message": "Can't Fetch from Database",
-            "error": error.message,
-        })
-    }
-})
-
-app.use('/', async (req, res) =>{
     res.status(200).json({
-        "status": true,
-        "message": "Ping Successfully!",
-    })
-})
+      status: true,
+      message: "get All Product Successfully!",
+      total: allProduct.length,
+      data: allProduct,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: false,
+      message: "Can't Fetch from Database",
+      error: error.message,
+    });
+  }
+});
+
+app.delete("/api/v1/categories/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const categoryToDelete = await category.findByPk(id);
+
+    if (!categoryToDelete) {
+      return res.status(404).json({
+        status: "Failed",
+        message: "Category not found!",
+        isSuccess: false,
+      });
+    }
+
+    await categoryToDelete.destroy();
+
+    return res.status(204).json({
+      status: "Success",
+      message: "Category deleted successfully!",
+      isSuccess: true,
+      data: null, 
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: "Error",
+      message: "Failed to delete category data!",
+      isSuccess: false,
+      error: error.message,
+    });
+  }
+});
+
+app.use("/", async (req, res) => {
+  res.status(200).json({
+    status: true,
+    message: "Ping Successfully!",
+  });
+});
 
 // middleware
 app.use((req, res, next) => {
-    res.status(404).json({
-        "status": false,
-        "message": "URL Not Found",
-        "data": null
-    });
+  res.status(404).json({
+    status: false,
+    message: "URL Not Found",
+    data: null,
+  });
 });
 
 app.listen(PORT, () => {
-    const url = `http://localhost:${PORT}`;
-    console.log(`Click to open:`, url);
-})
+  const url = `http://localhost:${PORT}`;
+  console.log(`Click to open:`, url);
+});
