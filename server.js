61512730199241ec730a9f001731d0f6dcbe21c6
AbdiPranawa <asaturaardana@gmail.com>
2024-10-08 19:57:53 +0700

    add app.get categories

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/api/v1/products", async (req, res) => {
+app.get("/api/v1/products", async (req, res) => {
   try {
     const allProduct = await product.findAll();
 
@@ -20,6 +20,32 @@ app.use("/api/v1/products", async (req, res) => {
       message: "get All Product Successfully!",
       total: allProduct.length,
       data: allProduct,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: false,
+      message: "Can't Fetch from Database",
+      error: error.message,
+    });
+  }
+});
+
+app.get("/api/v1/product/:id", async (req, res) => {
+  const productId = req.params.id;
+
+  try {
+    const { id } = req.params;
+    const productById = await product.findOne({ where: { id } });
+    if (!productById) {
+      return res.status(404).json({
+        status: false,
+        message: `Product with ID: ${id} not found`,
+      });
+    }
+    res.status(200).json({
+      status: true,
+      message: `Product with ID: ${id} fetched successfully!`,
+      data: productById,
     });
   } catch (error) {
     return res.status(500).json({
@@ -65,6 +91,50 @@ app.post("/api/v1/product", async (req, res) => {
   }
 });
 
+app.patch("/api/v1/product/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const { name, price, stock } = req.body;
+
+    if (!name || !price || !stock) {
+      return res.status(400).json({
+        status: false,
+        message: "Name, Price, and Stock are required!",
+      });
+    }
+
+    const productToUpdate = await product.findByPk(id);
+
+    if (!productToUpdate) {
+      return res.status(404).json({
+        status: false,
+        message: "Product not found!",
+      });
+    }
+
+    const updateProduct = await productToUpdate.update({
+      name,
+      price,
+      stock,
+    });
+
+    // Respond with success
+    res.status(200).json({
+      status: true,
+      message: "Product Updated Successfully!",
+      data: updateProduct,
+    });
+  } catch (error) {
+    // Handle any errors
+    return res.status(500).json({
+      status: false,
+      message: "Failed to update product",
+      error: error.message,
+    });
+  }
+});
+
 app.delete("/api/v1/product/:id", async (req, res) => {
   try {
     const productId = req.params.id;
@@ -73,6 +143,13 @@ app.delete("/api/v1/product/:id", async (req, res) => {
         id: productId,
       },
     });
+
+    if (deleteProduct === 0) {
+      return res.status(404).json({
+        status: false,
+        message: "Product Not Found!",
+      });
+    }
 
     res.status(200).json({
       status: true,
@@ -87,7 +164,7 @@ app.delete("/api/v1/product/:id", async (req, res) => {
   }
 });
 
-app.use("/api/v1/categories", async (req, res) => {
+app.get("/api/v1/categories", async (req, res) => {
   try {
     const allCategory = await category.findAll();
 
@@ -101,6 +178,62 @@ app.use("/api/v1/categories", async (req, res) => {
     return res.status(500).json({
       status: false,
       message: "Can't Fetch from Database",
+      error: error.message,
+    });
+  }
+});
+
+app.get("/api/v1/product/:id", async (req, res) => {
+  const categoryid = req.params.id;
+
+  try {
+    const { id } = req.params;
+    const categoryById = await category.findOne({ where: { id } });
+    if (!categoryById) {
+      return res.status(404).json({
+        status: false,
+        message: `Category with ID: ${id} not found`,
+      });
+    }
+    res.status(200).json({
+      status: true,
+      message: `Category with ID: ${id} fetched successfully!`,
+      data: categoryById,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: false,
+      message: "Can't Fetch from Database",
+      error: error.message,
+    });
+  }
+});
+
+app.post("/api/v1/category", async (req, res) => {
+  const { category_name, description } = req.body;
+
+  if (!category_name || !description) {
+    return res.status(400).json({
+      status: false,
+      message: "Category Name and Description required ",
+    });
+  }
+
+  try {
+    const newCategory = await category.create({
+      category_name: category_name,
+      description: description,
+    });
+
+    res.status(201).json({
+      status: true,
+      message: "Category Created Successfully!",
+      data: newCategory,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: false,
+      message: "Failed to Create Category",
       error: error.message,
     });
   }
@@ -122,7 +255,7 @@ app.delete("/api/v1/categories/:id", async (req, res) => {
 
     await categoryToDelete.destroy();
 
-    return res.status(204).json({
+    return res.status(200).json({
       status: "Success",
       message: "Category deleted successfully!",
       isSuccess: true,
