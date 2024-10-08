9d470aa8b12a8ae7a84d45b616411a587fd8e73a
haikalnuril <haikal.nuril23@gmail.com>
2024-10-08 18:57:30 +0700

    feat: delete handling product id not found

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/api/v1/products", async (req, res) => {
-
     try {
         const allProduct = await product.findAll();
 
@@ -40,14 +39,14 @@ app.post("/api/v1/product/:id", async (req, res) => {
         if (!name || !price || !stock) {
             return res.status(400).json({
                 status: false,
-                message: "Name, Price, and Stock are required!"
+                message: "Name, Price, and Stock are required!",
             });
         }
 
         const newProduct = await product.create({
             name,
             price,
-            stock
+            stock,
         });
 
         // Respond with success
@@ -68,17 +67,24 @@ app.post("/api/v1/product/:id", async (req, res) => {
 
 app.delete("/api/v1/product/:id", async (req, res) => {
     try {
-        const productId = req.params.id
+        const productId = req.params.id;
         const deleteProduct = await product.destroy({
             where: {
-                id: productId
-            }
-        })
+                id: productId,
+            },
+        });
+
+        if (deleteProduct === 0) {
+            return res.status(404).json({
+                status: false,
+                message: "Product Not Found!",
+            });
+        }
 
         res.status(200).json({
             status: true,
-            "message": "Product Deleted Successfully!"
-        })
+            message: "Product Deleted Successfully!",
+        });
     } catch (error) {
         return res.status(500).json({
             status: false,
@@ -89,40 +95,40 @@ app.delete("/api/v1/product/:id", async (req, res) => {
 });
 
 app.delete("/api/v1/categories/:id", async (req, res) => {
-  const id = req.params.id;
+    const id = req.params.id;
 
-  try {
-    const categoryToDelete = await category.findByPk(id);
+    try {
+        const categoryToDelete = await category.findByPk(id);
 
-    if (!categoryToDelete) {
-      return res.status(404).json({
-        status: "Failed",
-        message: "Category not found!",
-        isSuccess: false,
-      });
+        if (!categoryToDelete) {
+            return res.status(404).json({
+                status: "Failed",
+                message: "Category not found!",
+                isSuccess: false,
+            });
+        }
+
+        await categoryToDelete.destroy();
+
+        return res.status(204).json({
+            status: "Success",
+            message: "Category deleted successfully!",
+            isSuccess: true,
+            data: null,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: "Error",
+            message: "Failed to delete category data!",
+            isSuccess: false,
+            error: error.message,
+        });
     }
-
-    await categoryToDelete.destroy();
-
-    return res.status(204).json({
-      status: "Success",
-      message: "Category deleted successfully!",
-      isSuccess: true,
-      data: null, 
-    });
-  } catch (error) {
-    return res.status(500).json({
-      status: "Error",
-      message: "Failed to delete category data!",
-      isSuccess: false,
-      error: error.message,
-    });
-  }
 });
 
-app.patch("/api/v1/categories/:id", async(req,res)=>{
+app.patch("/api/v1/categories/:id", async (req, res) => {
     const id = req.params.id;
-    
+
     try {
         const categoryToUpdate = await category.findByPk(id);
 
@@ -141,7 +147,7 @@ app.patch("/api/v1/categories/:id", async(req,res)=>{
         // update category
         await categoryToUpdate.update({
             category_name: category_name || categoryToUpdate.category_name,
-            description: description || categoryToUpdate.description
+            description: description || categoryToUpdate.description,
         });
 
         return res.status(200).json({
@@ -165,12 +171,10 @@ app.use("/", async (req, res) => {
         status: true,
         message: "Ping Successfully!",
     });
-
 });
 
 // middleware
 app.use((req, res, next) => {
-
     res.status(404).json({
         status: false,
         message: "URL Not Found",
@@ -181,5 +185,4 @@ app.use((req, res, next) => {
 app.listen(PORT, () => {
     const url = `http://localhost:${PORT}`;
     console.log(`Click to open:`, url);
-
 });
